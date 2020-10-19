f77e60b31d95783941e0f26d25786db2e0d208d1
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-19 18:04:34 +0200

    finito

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,11 @@ var lista=['bianchi', 'rossi','balsano','giorgini' ];
 
 var utente=prompt('quale è il tuo cognome?')
 
+var xxx=lista.keys();
+for(var key of xxx){
+    console.log(key);
+}
+
 lista.push(utente)
 
 lista.sort();
